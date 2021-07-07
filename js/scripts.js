5a3c2aab5de99943fa81572fa0af161e09962edb
Kiernan2 <kiernan1994@gmail.com>
2021-07-07 14:42:50 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#zombie").click(function(){
+    $("#zombie-info").toggle();
+  });
+  $("#skeleton").click(function(){
+    $("#skeleton-info").toggle();
+  });
+  $("#vampire").click(function(){
+    $("#vampire-info").toggle();
+  });
+});
